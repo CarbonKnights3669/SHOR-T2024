2cd58f44c6079293cf448929a18dc7e06e1eb31e
Benjamin Schoolland <95189424+OBenjOne@users.noreply.github.com>
2024-05-17 12:57:23 -0700

    slow down intake

--- src/main/include/subsystems/IntakeShooter.h
+++ src/main/include/subsystems/IntakeShooter.h
@@ -125,8 +125,8 @@ private:
 	float I = 0;
 	float D = 0;
 	float F = -0.015;
-	float max = 0.4;
-	float min = -0.4;
+	float max = 0.25;
+	float min = -0.25;
 	rev::CANSparkMax m_intake{42, rev::CANSparkMax::MotorType::kBrushless};
 	rev::SparkPIDController intakePID = m_intake.GetPIDController();
 	rev::SparkRelativeEncoder e_intake = m_intake.GetEncoder(rev::SparkRelativeEncoder::Type::kHallSensor);
